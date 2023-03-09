cf5baeb79dd2c2c650810b0943e26c492cbe0f7a
dmitry filimonov <dmitry.filimonov@pluto.tv>
2023-03-10 01:52:27 +0300

    set-up libs

--- app/src/main/java/com/example/handycrypto/MainActivity.kt
+++ app/src/main/java/com/example/handycrypto/MainActivity.kt
@@ -1,11 +1,29 @@
 package com.example.handycrypto
 
-import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.tooling.preview.Preview
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContent {
+            messageCard()
+        }
+    }
+
+
+    @Composable
+    fun messageCard() {
+        Text(text = "hello")
+    }
+
+    @Preview
+    @Composable
+    fun PreviewMessageCard() {
+        messageCard()
     }
 }
